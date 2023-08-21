773e8cc9aa69bac847fc84c78e8f23894de4bb39
w-sst <142521737+w-sst@users.noreply.github.com>
2023-08-21 13:17:42 +0200

    init

---
+++ src/main/java/de/werum/coprs/cadip/cadip_mock/CadipMockApplication.java
@@ -0,0 +1,13 @@
+package de.werum.coprs.cadip.cadip_mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadipMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadipMockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/werum/coprs/cadip/cadip_mock/CadipMockApplicationTests.java
@@ -0,0 +1,13 @@
+package de.werum.coprs.cadip.cadip_mock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadipMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
