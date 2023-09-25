56e24f20a35dd494c436241f65729a362aeaf1fb
w-sst <142521737+w-sst@users.noreply.github.com>
2023-09-25 09:10:54 +0200

    added changes from cluster

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mvn clean package spring-boot:repackage
-docker build --build-arg proxy=$1 -t cadip_mock .
+docker build --build-arg proxy=$1 -t ghcr.io/w-fsi/cadip_mock .
+docker push ghcr.io/w-fsi/cadip_mock:latest
